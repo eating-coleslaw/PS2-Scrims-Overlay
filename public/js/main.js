85d16d68bcd7b8c6ba74c95cc9b27c21ffc9b902
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2018-08-21 23:45:59 -0500

    move event title to footer,  more player grid updates

--- public/js/main.js
+++ public/js/main.js
@@ -144,7 +144,7 @@ socket.on('connect', function() {
                     $('<div class="playerStatsContainer" id="' + m[keys].name + '">' +
                     '<div class="playerClass ' + getClassFromLoadoutID(m[keys].ps2Class) + '" id="' + m[keys].name + 'class"></div>' + 
                     '<div class="playerStatsName" id="' + m[keys].name + 'name">' + m[keys].name + '</div>' +
-                    '<div class="playerEventMask" id="' + m[keys].name + 'EventMask">' + '<div class="strip"></div><div class="strip"></div>' +
+                    '<div class="playerEventMask" id="' + m[keys].name + 'EventMask">' +
                     '<div class="playerRespawningBase" id="' + m[keys].name + 'respawn"></div>'+ '</div>' + '</div>').appendTo($('#T2Players'));
                 } else {
                     var scoreEl = document.getElementById(m[keys].name + 'score');
